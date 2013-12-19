2226cbf8288a9753a0cf76b0875b86fad1d7e4b6
acin91 <z.aliakseyeu@gmail.com>
2013-12-19 13:04:38 +0300

    added dependency on nexmo sms gem

--- lib/revisor/notification/email.rb
+++ lib/revisor/notification/email.rb
@@ -2,7 +2,7 @@ module Revisor
     module Notification
         class Email
             def initialize(config)
-
+                
             end
 
             def notify(body)
--- lib/revisor/notification/sms.rb
+++ lib/revisor/notification/sms.rb
@@ -1,12 +1,22 @@
+require 'nexmo'
+
 module Revisor
     module Notification
         class Sms
-            def initialize(config)
 
+            def initialize(config)
+                @client = Nexmo::Client.new(config["api_key"], config["api_secret"])
+                @phone_number = config["phone_number"]
+                @from = config["from"]
             end
 
             def notify(body)
-                puts body
+                response = @client.send_message({:to => @phone_number, :from => @from, :text => body})
+
+                if !response.ok?
+                    puts "Sms sending is broken"
+                    # here must be logger if we talk about good practices
+                end
             end
         end
     end
